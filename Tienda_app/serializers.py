50b93bf151b083dd752936775b03ac3b72870e3f
JuanPabloVelozo <jpvelozo219@gmail.com>
2025-08-29 23:53:58 -0400

    ajustes de crud

--- Tienda_app/serializers.py
+++ Tienda_app/serializers.py
@@ -1,42 +1,92 @@
-from rest_framework import serializers
+﻿from rest_framework import serializers
 from .models import Marca, Categoria, Producto, ProductoImagen, DireccionEnvio, Orden, DetalleOrden, Resena
 
 class MarcaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Marca
         fields = '__all__'
+        read_only_fields = ('id',)
+
 
 class CategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = '__all__'
+        read_only_fields = ('id',)
+
 
 class ProductoSerializer(serializers.ModelSerializer):
+    categoria = serializers.PrimaryKeyRelatedField(queryset=Categoria.objects.all())
+    marca = serializers.PrimaryKeyRelatedField(queryset=Marca.objects.all())
+
     class Meta:
         model = Producto
         fields = '__all__'
+        read_only_fields = ('id', 'fecha_creacion')
+
+    def validate_precio(self, value):
+        if value < 0:
+            raise serializers.ValidationError("El precio no puede ser negativo")
+        return value
+
+    def validate_stock(self, value):
+        if value < 0:
+            raise serializers.ValidationError("El stock no puede ser negativo")
+        return value
+
 
 class ProductoImagenSerializer(serializers.ModelSerializer):
+    producto = serializers.PrimaryKeyRelatedField(queryset=Producto.objects.all())
+
     class Meta:
         model = ProductoImagen
         fields = '__all__'
+        read_only_fields = ('id',)
+
 
 class DireccionEnvioSerializer(serializers.ModelSerializer):
+    usuario = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = DireccionEnvio
         fields = '__all__'
+        read_only_fields = ('id',)
+
 
 class OrdenSerializer(serializers.ModelSerializer):
+    usuario = serializers.PrimaryKeyRelatedField(read_only=True)
+
     class Meta:
         model = Orden
         fields = '__all__'
+        read_only_fields = ('id', 'fecha_creacion', 'estado')
+
 
 class DetalleOrdenSerializer(serializers.ModelSerializer):
+    orden = serializers.PrimaryKeyRelatedField(queryset=Orden.objects.all())
+    producto = serializers.PrimaryKeyRelatedField(queryset=Producto.objects.all())
+
     class Meta:
         model = DetalleOrden
         fields = '__all__'
+        read_only_fields = ('id',)
+
+    def validate_cantidad(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("La cantidad debe ser mayor que cero")
+        return value
+
 
 class ResenaSerializer(serializers.ModelSerializer):
+    usuario = serializers.PrimaryKeyRelatedField(read_only=True)
+    producto = serializers.PrimaryKeyRelatedField(queryset=Producto.objects.all())
+
     class Meta:
         model = Resena
         fields = '__all__'
+        read_only_fields = ('id', 'fecha_creacion')
+
+    def validate_calificacion(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("La calificación debe estar entre 1 y 5")
+        return value
